5358db92816a078bcedaf0939cf19c832f8af360
Jimena <jparatge@gmail.com>
2025-01-10 04:09:25 +0000

    home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import phLogo from "../../img/logo-ph1.jpg";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src={phLogo} width="80"/>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-primary">Login</button>
 					</Link>
 				</div>
 			</div>
