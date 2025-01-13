9f81e49ea5021dfe483d100ea5b128f7c3e93bf8
Jimena <jparatge@gmail.com>
2025-01-13 03:21:57 +0000

    home 2

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,12 +8,52 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Playa Hermosa</h1>
-			<p>
-				<img src={phImageUrl}/>
-			</p>
-			
-			
+
+			<section className="about-us">
+				<h1>Playa Hermosa - Maldonado, Uruguay</h1>
+
+				<div className="about-us-cards">
+					<div className="card">
+						<div className="card-body">
+							<h4 className="card-title">Paseos</h4>
+						</div>
+					</div>
+					<div className="card">
+						<div className="card-body">
+							<h4 className="card-title">Galería</h4>
+							<p className="card-text">
+
+							</p>
+						</div>
+					</div>
+					<div className="card">
+						<div className="card-body">
+							<h4 className="card-title">Alojamiento</h4>
+						</div>
+					</div>
+					<div className="card">
+						<div className="card-body">
+							<h4 className="card-title">Cómo llegar</h4>
+						</div>
+					</div>
+					<div className="card">
+						<div className="card-body">
+							<h4 className="card-title">Novedades</h4>
+						</div>
+					</div>
+					<div className="card">
+						<div className="card-body">
+							<h4 className="card-title">Contacto</h4>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			{/* <p>
+				<img src={phImageUrl} />
+			</p> */}
+
+
 		</div>
 	);
 };
